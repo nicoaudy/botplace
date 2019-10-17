70cdaf5f7c08271e2348e60bb25315fe530e8341
Nico Audy <nicoaudy1994@gmail.com>
2019-10-17 11:42:38 +0700

    ah setup done

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class SettingController extends Controller
 {
     public function index()
     {
-        return 'Setting';
+        Telegram::sendMessage([
+            'chat_id' => '280560002',
+            'text' => 'Something'
+        ]);
+
+        // $response = Telegram::getMe();
+
+        // $botId = $response->getId();
+        // $firstName = $response->getFirstName();
+        // $username = $response->getWebhookUpdates();
+
+        dd($response = Telegram::getUpdates());
     }
 }
